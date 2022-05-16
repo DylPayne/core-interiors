0f9a08a25c8cc3f8edeaa68425de97a84eca6b7d
Dylan Payne <dylan@rhys.co.za>
2022-05-16 11:45:23 +0200

    trying another SSR fix

--- pages/services.js
+++ pages/services.js
@@ -1,17 +1,19 @@
 import Layout from "../components/layout/layout";
 import Services from "../components/services/services";
 import HeaderSection from "../components/header-section/headerSection";
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
 
 export default function ServicesPage() {
   return (
     <main>
-      <Layout>
-        <HeaderSection
-          title="Services"
-          subtitle="Take a look at some of our services"
-        />
-        <Services />
-      </Layout>
+      <Header />
+      <HeaderSection
+        title="Services"
+        subtitle="Take a look at some of our services"
+      />
+      <Services />
+      <Footer />
     </main>
   );
 }
